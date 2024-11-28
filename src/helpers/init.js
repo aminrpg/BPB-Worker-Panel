55a71663648c3f24a7a6f9b00f49e73375cda636
Amin Sepehri <aminrpg@gmail.com>
2024-11-28 15:10:31 +0330

    test-clf-bypass

--- src/helpers/init.js
+++ src/helpers/init.js
@@ -4,7 +4,7 @@ let userID, dohURL, proxyIP, trojanPassword, defaultHttpPorts, defaultHttpsPorts
 
 function initParams(request, env) {
     const proxyIPs = env.PROXYIP?.split(',').map(proxyIP => proxyIP.trim());
-    userID = env.UUID || '89b3cbba-e6ac-485a-9481-976a0415eab9';
+    userID = env.UUID || 'e121cdfc-401e-42be-aa4f-c4293544d087';
     if (!isValidUUID(userID)) throw new Error(`Invalid UUID: ${userID}`);
     dohURL = env.DOH_URL || 'https://cloudflare-dns.com/dns-query';
     proxyIP = proxyIPs ? proxyIPs[Math.floor(Math.random() * proxyIPs.length)] : defaultProxyIP;
